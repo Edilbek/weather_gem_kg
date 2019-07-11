4c11a8750b1c978a70e8d5312c974c34a44e87e7
Edilbek <edil.talantbekov@gmail.com>
2019-07-11 15:46:52 +0600

    remove modules and refactor code

--- lib/weather_gem_kg.rb
+++ lib/weather_gem_kg.rb
@@ -1,39 +1,17 @@
 # require "weather_kg/version"
 require "httparty"
 
-module ApiXu
-  class TodayWeather
-    def initialize(city)
-      @city = city
-      @data =  HTTParty.get("http://api.apixu.com/v1/forecast.json?key=6d79fff9ef764b8c85d61836190907&q=#{@city}&days=5")
-    end
-    def location
-      @data["location"]["name"]
-    end
-    
-    def temperature
-      @temper = @data["current"]["temp_c"]
-      @temper.to_i
-    end
-    
-    def time
-      @time = @data["location"]["localtime"]
-      now_time = Time.parse(@time)
-      now_time.ctime.slice(0, 16)
-    end
-
-    def condition
-      @data["current"]["condition"]["text"]      
-    end    
+class Apixu
+  def initialize(city, day_count)
+    @city = city
+    @day_count = day_count
+    # @api = api
+    @data = HTTParty.get("http://api.apixu.com/v1/forecast.json?key=6d79fff9ef764b8c85d61836190907&q=#{@city}&days=#{day_count}")
   end
-  
-  class FiveDayWeather
-    def initialize(city)
-      @city = city
-      @data =  HTTParty.get("http://api.apixu.com/v1/forecast.json?key=6d79fff9ef764b8c85d61836190907&q=#{@city}&days=5")
-    end
-    def fiveday_weather
-      five = []
+
+  def get_weather
+    weather_data = []
+    if @day_count >= 2
       one = []
       days = @data["forecast"]["forecastday"]
       location = @data["location"]["name"]
@@ -41,52 +19,55 @@ module ApiXu
         today = day["date_epoch"]
         one << Time.at(today).ctime.slice(0, 10)
         one << day["day"]["maxtemp_c"].to_i
-        one << day["day"]["mintemp_c"].to_i 
+        one << day["day"]["mintemp_c"].to_i
         one << day["day"]["condition"]["text"]
       end
       one.each_slice(4) do |i|
-        five << i
+        weather_data << i
       end
-      return five
+      # return data
+    else
+      location = @data["location"]["name"]
+      temper = @data["current"]["temp_c"].to_i
+
+      time = @data["location"]["localtime"]
+      now_time = Time.parse(time).ctime.slice(0, 16)
+
+      condition = @data["current"]["condition"]["text"]
+
+      weather_data << location
+      weather_data << temper
+      weather_data << now_time
+      weather_data << condition
     end
+    return weather_data
   end
 end
 
-module DarkSky
-  class TodayWeather
-    def initialize(city)
-      @city = city
-      @data = HTTParty.get("https://api.darksky.net/forecast/0a06f48b71537e4e72554c3f8d39d234/#{@city}")
-    end
-    def location
-      @data["timezone"]
-    end
-    
-    def temperature
-      farenheit = @data["currently"]["temperature"]
-      celsius = (farenheit - 32) * 5/9
-      celsius.to_i
-    end
-    
-    def time
-      time = @data["currently"]["time"]
-      local_time = Time.at(time)
-      local_time.ctime.to_s.slice(0, 16)
-    end
-
-    def condition
-      @data["currently"]["summary"]    
-    end  
+class DarkSky
+  def initialize(city, time)
+    @city = city
+    @time = time
+    @data = HTTParty.get("https://api.darksky.net/forecast/0a06f48b71537e4e72554c3f8d39d234/#{@city}")
+    @data["#{@time}"]
   end
 
-  class FiveDayWeather
-    def initialize(city)
-      @city = city
-      @data = HTTParty.get("https://api.darksky.net/forecast/0a06f48b71537e4e72554c3f8d39d234/#{@city}")
-    end
-  
-    def fiveday_weather
-      five = []
+  def get_weather
+    weather_data =[]
+    if @time == 'currently'
+      timezone = @data["timezone"]
+      farenheit = @data["currently"]["temperature"]
+      celsius = ((farenheit - 32) * 5/9).to_i
+
+      time = @data["currently"]["time"]
+      local_time = Time.at(time).ctime.to_s.slice(0, 16)
+
+      condition = @data["currently"]["summary"]
+      weather_data << timezone
+      weather_data << celsius
+      weather_data << local_time
+      weather_data << condition
+    else
       one = []
       location = @data["timezone"]
       days = @data["daily"]["data"]
@@ -102,32 +83,13 @@ module DarkSky
         one << high_celsius.to_i
         one << low_celsius.to_i
         one << day["summary"]
-      end 
-      one.each_slice(4) do |i|
-        five << i
       end
-      return five
+      one.each_slice(4) do |i|
+        weather_data << i
+      end
     end
+    return weather_data
   end
 end
-# var = ApiXu::TodayWeather.new("Talas")
-# var.location
-# var.temperature
-
-# var = ApiXu::FiveDayWeather.new("Bishkek")
-# var.fiveday_weather
-# ApiXu::TodayWeather.location
-# puts ApiXu::TodayWeather.location
-# puts ApiXu::TodayWeather.temperature
-# puts ApiXu::TodayWeather.time
-# puts ApiXu::TodayWeather.condition
-# puts '----'
-# var = DarkSky::FiveDayWeather.new("42.87,74.6")
-# var.fiveday_weather
-# var.location
-# var.temperature
-# DarkSky::FiveDayWeather.fiveday_weather
-# puts DarkSky::TodayWeather.location
-# puts DarkSky::TodayWeather.temperature
-# puts DarkSky::TodayWeather.time
-# puts DarkSky::TodayWeather.condition
+# weather = DarkSky.new("42.88,74.58", "daily")
+# puts weather.get_weather.count
