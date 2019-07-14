60624bfcd012f3f542f9e44ca69d413d542fa37f
Edilbek <edil.talantbekov@gmail.com>
2019-07-14 16:24:50 +0600

    commented lines and changed version

--- lib/weather_gem_kg.rb
+++ lib/weather_gem_kg.rb
@@ -5,8 +5,8 @@ require "weather_gem_kg/generator"
 module WeatherGemKg
 end
 
-weather1 = WeatherGemKg::Generator.new('Bishkek', 1, WeatherGemKg::Clients::Apixu.new)
-weather1.generate_me
+# weather1 = WeatherGemKg::Generator.new('Bishkek', 1, WeatherGemKg::Clients::Apixu.new)
+# weather1.generate_me
 #
 # weather2 = WeatherGemKg::Generator.new('Bishkek', 1, WeatherGemKg::Clients::DarkSky.new)
 # puts weather2.generate_me
