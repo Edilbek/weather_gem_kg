a7de8ef7655b4e8cbcde1107af734c264983d02e
Edilbek <edil.talantbekov@gmail.com>
2019-07-15 02:00:31 +0600

    remove gem version

--- lib/weather_gem_kg/config.rb
+++ lib/weather_gem_kg/config.rb
@@ -13,8 +13,3 @@ module WeatherGemKg
     @config ||= Config.new
   end
 end
-
-WeatherGemKg.configure do |c|
-  c.api_key_apixu = '6d79fff9ef764b8c85d61836190907'
-  c.api_key_dark = '0a06f48b71537e4e72554c3f8d39d234'
-end
