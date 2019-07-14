074f09cb79a729e15bfcf344bcc8ee80344dd712
Edilbek <edil.talantbekov@gmail.com>
2019-07-14 19:12:41 +0600

    add namespace

--- lib/weather_gem_kg/config.rb
+++ lib/weather_gem_kg/config.rb
@@ -1,11 +1,18 @@
-class WeatherGem
-  Config = Struct.new :api_key_apixu, :api_key_dark  # => MyGem::Config
 
-  def self.configure(&config_block)
-    config_block.call config         # => "some string"
-  end                                # => :configure
+module WeatherGemKg
+  class Configuration
+    Config = Struct.new :api_key_apixu, :api_key_dark
 
-  def self.config
-    @config ||= Config.new  # => #<struct MyGem::Config property1=nil, property2=nil>, #<struct MyGem::Config property1=1, property2="some string">
-  end                       # => :config
-end                         # => :config
+    def self.configure(&config_block)
+      config_block.call config
+    end
+
+    def self.config
+      @config ||= Config.new
+    end
+  end
+end
+WeatherGemKg::Configuration.configure do |c|
+  c.api_key_apixu = '6d79fff9ef764b8c85d61836190907'
+  c.api_key_dark = '0a06f48b71537e4e72554c3f8d39d234'
+end
