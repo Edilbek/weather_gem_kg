baebabc070fc6934e88f1864305f15e6058b64da
Edilbek <edil.talantbekov@gmail.com>
2019-07-15 21:33:35 +0600

    fin commit of config

--- lib/weather_gem_kg/config.rb
+++ lib/weather_gem_kg/config.rb
@@ -20,7 +20,3 @@ module WeatherGemKg
     end
   end
 end
-WeatherGemKg.configure do |c|         # => MyGem
-  c.api_key_apixu = '6d79fff9ef764b8c85d61836190907'              # => 1
-  c.api_key_dark = '0a06f48b71537e4e72554c3f8d39d234'  # => "some string"
-end                            # => "some string"
